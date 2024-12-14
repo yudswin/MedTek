df76bdec7f62162d5d53d8fe63c744d6577f4d8f
Lê Đăng Khoa <Khoakhoa2812@gmail.com>
2024-12-14 10:14:46 +0700

    change method name (WeatherDisplay -> GreetingDisplay)

--- app/src/main/java/com/medtek/main/greeting/greetingScreen.kt
+++ app/src/main/java/com/medtek/main/greeting/greetingScreen.kt
@@ -1,26 +1,12 @@
 package com.medtek.main.greeting
 
-import android.content.Context
-import android.graphics.Bitmap
-import android.os.Bundle
-import android.widget.Toast
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.FormatQuote
-import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ComposeScene
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -29,27 +15,13 @@ import com.medtek.main.greeting.Presentation.Components.CloseBar
 import com.medtek.main.greeting.Presentation.Components.WeatherCard
 import com.medtek.main.greeting.Presentation.Components.locationText
 import com.medtek.main.greeting.Presentation.Components.quoteCard
-import java.io.File
-import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.Density
-import androidx.compose.ui.unit.IntSize
-import com.medtek.main.ui.theme.AppTheme
-import java.io.FileOutputStream
-import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
-import androidx.compose.ui.graphics.Canvas
 import androidx.compose.ui.graphics.Color
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.*
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.unit.dp
 
 
 @Preview
 @Composable
-fun WeatherDisplay() {
+fun GreetingDisplay() {
     Scaffold(
         modifier = Modifier.fillMaxSize()
     ) { innerPadding ->
