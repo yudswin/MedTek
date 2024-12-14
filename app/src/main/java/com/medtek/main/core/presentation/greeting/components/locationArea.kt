76c393218e786d02fbda7dd725623999337e39f0
Lê Đăng Khoa <Khoakhoa2812@gmail.com>
2024-12-14 20:29:42 +0700

    update directory

--- app/src/main/java/com/medtek/main/core/presentation/greeting/components/locationArea.kt
+++ app/src/main/java/com/medtek/main/core/presentation/greeting/components/locationArea.kt
@@ -1,30 +1,33 @@
-package com.medtek.main.greeting.Presentation.Components
+package com.medtek.main.core.presentation.greeting.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Preview
 @Composable
-fun quoteCard(){
+fun locationText(){
     Row (
         modifier = Modifier.fillMaxWidth(),
         verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.Start
+        horizontalArrangement = Arrangement.Center
     ) {
         Box(
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxWidth(),
+            contentAlignment = Alignment.Center
         ) {
             Text (
-                modifier = Modifier,
-                text = "Literature adds to reality, it does not simply describe it. It enriches the necessary competencies that daily life requires and provides; and in this respect, it irrigates the deserts that our lives have already become.",
+                modifier = Modifier.size(width = 300.dp, height = 40.dp),
+                text = "District 3, Ho Chi Minh City",
                 fontSize = 24.sp
             )
         }
--- app/src/main/java/com/medtek/main/core/presentation/greeting/components/weatherArea.kt
+++ app/src/main/java/com/medtek/main/core/presentation/greeting/components/weatherArea.kt
@@ -1,12 +1,10 @@
-package com.medtek.main.greeting.Presentation.Components
+package com.medtek.main.core.presentation.greeting.components
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
@@ -15,11 +13,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.DrawScope
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.medtek.main.R
 
 @Preview
 @Composable
